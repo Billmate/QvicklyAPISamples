09e97e739019b488702289605a3a3da3fb665507
Thomas Björk <107463939+codedeviate@users.noreply.github.com>
2024-06-12 14:53:55 +0200

    Updated code versions

--- PHP/src/Qvickly/PaymentAPI.php
+++ PHP/src/Qvickly/PaymentAPI.php
@@ -12,7 +12,7 @@
  * @author Thomas Björk <thomas.bjork@qvickly.io>
  * @copyright 2013-2024 Billmate AB
  * @license Proprietary and fully owned by Billmate AB
- * @version 1.0.0
+ * @version 3.0.0
  * @link http://www.qvickly.io
  *
  * History:
@@ -49,7 +49,7 @@ class PaymentAPI
         $this->test = $test;
         $this->referer = $referer;
         
-        defined('QVICKLY_CLIENT') || define('QVICKLY_CLIENT', 'Qvickly:PHPLegacy:2.2.0');
+        defined('QVICKLY_CLIENT') || define('QVICKLY_CLIENT', 'Qvickly:PHP:3.0.0');
         defined('QVICKLY_SERVER') || define('QVICKLY_SERVER', '2.3.0');
         defined('QVICKLY_LANGUAGE') || define('QVICKLY_LANGUAGE', '');
     }
